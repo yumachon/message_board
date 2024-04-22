bf106bb9dcb1b996a9d4903dc6ad3f9af750c447
yuma.takushima <takushima@valuedesign.jp>
2024-04-22 16:10:33 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -43,6 +43,10 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
+        if(m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
+
         // メッセージIDをセッションスコープに登録
         request.getSession().setAttribute("message_id", m.getId());
 
