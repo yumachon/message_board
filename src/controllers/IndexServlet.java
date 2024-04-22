03259c962e070b5b7a50d7c381c1cbe8dda10570
yuma.takushima <takushima@valuedesign.jp>
2024-04-22 13:51:04 +0900

    CreateServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -29,7 +29,7 @@ public class IndexServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    /**
+    /**-0
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
